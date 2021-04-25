a8a70c5b153f44733e7b146f87ec77dcefce509e
Pavel Soloshenko <apodisation13@gmail.com>
2021-04-25 12:39:14 +0300

    init

---
+++ application/db/people.py
@@ -0,0 +1,6 @@
+def get_employees():
+    print('Список сотрудников: ')
+
+
+if __name__ == '__main__':
+    print('модуль people.py')
---
+++ application/salary.py
@@ -0,0 +1,5 @@
+def calculate_salary():
+    print('Суммарная зарплата = ')
+
+
+print('модуль salary.py')
